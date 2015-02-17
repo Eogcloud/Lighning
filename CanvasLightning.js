e22cd629c405d32d615e9f3075a7684694bdb621
Eogcloud <eogcloud@gmail.com>
2015-02-17 12:49:29 +0000

    Initial Commit
    Added HTML and JS

---
+++ CanvasLightning.js
@@ -0,0 +1,40 @@
+var context = document.getElementsByTagName('canvas')[0].getContext('2d');
+var lastX = context.canvas.width * Math.random();
+var lastY = context.canvas.height * Math.random();
+var hue = 0;
+
+function line() {
+    context.save();
+    for (var j = 0; j < 20; j++) {
+        context.beginPath();
+        context.lineWidth = Math.random();
+        lastX = context.canvas.width * Math.random();
+        lastY = 0;
+
+        context.moveTo(lastX, lastY);
+        for (var i = 0; lastY < context.canvas.height; i++) {
+            context.lineTo(
+              lastX = lastX + 15 * Math.random() - 15 * Math.random(),
+              lastY = lastY + 10 * Math.random() - 1 * Math.random()
+            );
+        }
+
+        context.strokeStyle = 'white';
+        context.shadowColor = 'white';
+        context.shadowBlur = 10;
+        context.stroke();
+    }
+    context.restore();
+}
+
+function clear() {
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+function blank() {
+    context.fillStyle = 'rgba(0,0,0,0.1)';
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+}
+
+setInterval(line, 500);
+setInterval(blank, 30);
